396ed0af200ac4ca61bc92270622949154260a7a
takecourage-co <bart@takecourage.co>
2025-05-29 17:44:56 +0100

    Update sc-nav.js

--- sc-nav.js
+++ sc-nav.js
@@ -294,7 +294,3 @@ document.addEventListener("DOMContentLoaded", () => {
   });
 });
 </script>
-
-<script src="https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=65ca0469e50425dbb79802ac" type="text/javascript" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
-<script src="https://cdn.prod.website-files.com/65ca0469e50425dbb79802ac/js/webflow.schunk.36b8fb49256177c8.js" type="text/javascript"></script>
-<script src="https://cdn.prod.website-files.com/65ca0469e50425dbb79802ac/js/webflow.3d569bf6.391759af8c42d475.js" type="text/javascript"></script>
